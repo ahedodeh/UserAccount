aa9d1808ed734fa70cd42e49b9faf703b94463ec
unknown <ahedodeh@outlook.com>
2024-03-20 09:59:57 +0200

    Create log in with jwt

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,17 +7,40 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware("auth:api", ["except" => ["register", "login"]]);
+        $this->middleware("auth:api", ["except" => ["login"]]);
     }
 
-    
-    public function login(Request $request)
-    {
-        
+
+    public function login(LoginRequest $request)
+{
+    $credentials = $request->only('email', 'password');
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+        $token = $this->createNewToken($user);
+        return response()->json(['message' => 'Login successful','token' => $token]);
+    } else {
+        return response()->json(['message' => 'Invalid credentials'], 401);
     }
+}
+
+    public function createNewToken($user)
+    {
+        $token = JWTAuth::fromUser($user);
+        $jwtTTL = JWTAuth::factory()->getTTL();
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $jwtTTL * 60,
+            'user' => $user,
+        ];
+    }
+
 }
